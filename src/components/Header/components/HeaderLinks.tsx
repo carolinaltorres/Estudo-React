8004c43c6c2f9163467a3f39e31abbf8c3d26769
Carolina <carolinadelimatorres@gmail.com>
2026-01-12 00:09:19 -0300

    feat:  novos componentes

---
+++ src/components/Header/components/HeaderLinks.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MenuList from '../../Menu/MenuList'
+import MenuItem from '../../Menu/MenuItem'
+
+const HeaderLinks = () => {
+  return (
+    <MenuList>
+      <MenuItem>Eventos</MenuItem>
+      <MenuItem>Clube fidelidade</MenuItem>
+      <MenuItem>Spbre nós</MenuItem>
+    </MenuList>
+  )
+}
+
+export default HeaderLinks
